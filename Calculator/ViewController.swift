b23421082c2ace5af2e0bfd67fd7c9c5238ea989
YiQiZhang <jerry_techtree@126.com>
2015-07-20 23:22:23 +0800

    Updated 完善的计算器功能

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,5 +24,51 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    var numberStack = [Double]()
+    @IBAction func enter() {
+        numberStack.append(displayVal)
+        isInit = false
+        print(numberStack)
+    }
+    
+    // computed properties 计算型属性
+    var displayVal: Double {
+        get {
+            return NSNumberFormatter().numberFromString(calDisplay.text!)!.doubleValue
+        }
+        set {
+            calDisplay.text = "\(newValue)"
+            isInit = false
+        }
+    }
+    
+    @IBAction func operatorAction(sender: UIButton) {
+        let operatorNotice = sender.currentTitle!
+        if isInit {
+            enter()
+        }
+        switch operatorNotice{
+        case "+":
+            getOperaRes({ $0 + $1 })
+        case "-":
+            getOperaRes({ $1 - $0 })
+        case "*":
+            getOperaRes({ (op1, op2) -> Double in
+                op1 * op2
+            })
+        case "/":
+            getOperaRes({ $1 / $0})
+        default:
+            break
+        }
+    }
+    
+    func getOperaRes(operaFunc: (Double , Double) -> Double){
+        if numberStack.count >= 2 {
+            displayVal = operaFunc(numberStack.removeLast(), numberStack.removeLast())
+            enter()
+        }
+    }
 }
 
